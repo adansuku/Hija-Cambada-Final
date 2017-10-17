9fec3cfc10d3bd3b548a39458116aee4b0e174a2
adansuku <adangrx@gmail.com>
2017-10-17 12:58:04 +0100

    cambios

--- Hija Cambada V.1.0/platforms/android/assets/www/js/esquema/esquemaController.js
+++ Hija Cambada V.1.0/platforms/android/assets/www/js/esquema/esquemaController.js
@@ -1,1 +1,17 @@
-angular.module("starter.esquemaController",[]).controller("EsquemaCtrl",["$scope","$ionicHistory","$state",function(o,t,r){o.atrasEsquema=function(){window.history.back()}}]);
+angular.module('starter.esquemaController', [])
+
+// Controller para manejar los datos de los esquemas
+.controller('EsquemaCtrl', function($scope, $ionicHistory, $state) {
+
+	// Funcion para volver atras desde los pajaros.
+  $scope.atrasEsquema = function()
+	{
+	    window.history.back();
+	}
+
+
+}) // Fin del controller
+
+
+
+; // Fin de starter.avesController
