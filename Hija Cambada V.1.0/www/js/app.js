b09e0624d812616b81630c0232e42eb745dc01f9
Ulises <uliseslevi@gmail.com>
2017-10-17 10:39:39 +0100

    Version 1.4.13

--- Hija Cambada V.1.0/www/js/app.js
+++ Hija Cambada V.1.0/www/js/app.js
@@ -788,7 +788,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.plantasContr
     url: '/principalMapa',
     views: {
       'menuContent': {
-        templateUrl: './../templates/mapa/principalMapa.html',
+        templateUrl: 'templates/mapa/principalMapa.html',
         controller: 'MapaCtrl'
       }
     }
@@ -798,7 +798,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.plantasContr
     url: '/principalMapaMenu',
     views: {
       'menuContent': {
-        templateUrl: './../templates/mapa/principalMapaMenu.html',
+        templateUrl: 'templates/mapa/principalMapaMenu.html',
         controller: 'MapaCtrl'
       }
     }
