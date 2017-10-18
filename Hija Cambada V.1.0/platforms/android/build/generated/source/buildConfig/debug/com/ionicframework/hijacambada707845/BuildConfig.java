f0f5dc18ccda4608a915868da1e9b4cebf4a1df3
Ulises <uliseslevi@gmail.com>
2017-10-18 11:56:27 +0100

    ultimos cambios en android, la verison subida

--- Hija Cambada V.1.0/platforms/android/build/generated/source/buildConfig/debug/com/ionicframework/hijacambada707845/BuildConfig.java
+++ Hija Cambada V.1.0/platforms/android/build/generated/source/buildConfig/debug/com/ionicframework/hijacambada707845/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.ionicframework.hijacambada707845";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 10413;
-  public static final String VERSION_NAME = "1.4.13";
+  public static final int VERSION_CODE = 10414;
+  public static final String VERSION_NAME = "1.4.14";
 }
